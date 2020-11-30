665cad14f17b05ba230f3c1d5bdf9ed42b00f8e2
Ty Burns <tyburns@MacBook-Pro-2.local>
2020-11-30 16:54:59 -0500

    Created a directory for components and added some simple members to it

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ToDosComponent } from './components/to-dos/to-dos.component';
+import { ListComponent } from './components/list/list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToDosComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule,
