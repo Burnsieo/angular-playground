2ef67705328914ff150fd27ebc0f744239b33510
Ty Burns <tyburns@MacBook-Pro-2.local>
2020-11-30 17:07:01 -0500

    Created a folder for models and added a Todo model to it

--- src/app/components/to-dos/to-dos.component.ts
+++ src/app/components/to-dos/to-dos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Todo } from '../../models/Todo';
 
 @Component({
   selector: 'app-to-dos',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./to-dos.component.scss']
 })
 export class ToDosComponent implements OnInit {
+  todos:Todo[];
 
   constructor() { }
 
