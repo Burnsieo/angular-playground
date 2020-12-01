f43a00e6310672c6b5185dcc335576d936d8633d
Ty Burns <tyburns@MacBook-Pro-2.local>
2020-12-01 13:50:23 -0500

    Added a home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ToDosComponent } from './components/to-dos/to-dos.component';
 import { ListComponent } from './components/list/list.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ToDosComponent,
-    ListComponent
+    ListComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
