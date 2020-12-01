0368ce607a8d6613d96a39949dce46e8b4aec1e9
Ty Burns <tyburns@MacBook-Pro-2.local>
2020-12-01 14:09:21 -0500

    Added routing for home, list, and todo components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { ListComponent } from './components/list/list.component';
+import { ToDosComponent } from './components/to-dos/to-dos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'list', component: ListComponent},
+  {path: 'ToDo', component: ToDosComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
